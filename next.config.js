f24d7ce1a919564df64e357a58918298b44d6f2c
Daniel Restrepo <jdrestrepo@unitecnica.net>
2024-01-28 21:30:01 -0500

    Create ui to show pokemon

--- next.config.js
+++ next.config.js
@@ -5,9 +5,16 @@ const nextConfig = {
             {
                 protocol: 'https',
                 hostname: 'images.unsplash.com'
+            },
+            {
+                protocol: 'https',
+                hostname: 'raw.githubusercontent.com'
             }
         ]
     }
 }
 
 module.exports = nextConfig
+
+/* Error: Invalid src prop (https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png) on `next/image`, hostname "raw.githubusercontent.com" is not configured under images in your `next.config.js`
+See more info: https://nextjs.org/docs/messages/next-image-unconfigured-host */
