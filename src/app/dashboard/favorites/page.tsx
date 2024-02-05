d74fa39b0987fda2490158ba8ee629d70b6f1a67
Daniel Restrepo <jdrestrepo@unitecnica.net>
2024-02-04 22:42:42 -0500

    Configure redux toolkit

---
+++ src/app/dashboard/favorites/page.tsx
@@ -0,0 +1,31 @@
+import { PokemonsList, PokemonsResponse, SimplePokemon } from "@/pokemons";
+
+
+const getPokemons = async (
+  limit = 20,
+  offset = 0
+): Promise<SimplePokemon[]> => {
+  const data: PokemonsResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+  ).then((res) => res.json());
+
+  const pokemons = data.results.map((pokemon) => ({
+    id: pokemon.url.split("/").at(-2)!,
+    name: pokemon.name,
+  }));
+
+  return pokemons;
+};
+
+export default async function PokemonsPage() {
+  const pokemons = await getPokemons(151);
+
+  return (
+    <div className="flex flex-col">
+      <span className="text-5lx my-2">Pokemons favoritos <small>Global staate</small></span>
+      <div className="flex flex-wrap gap-10 items-center justify-center">
+        <PokemonsList pokemons={pokemons}/>
+      </div>
+    </div>
+  );
+}
